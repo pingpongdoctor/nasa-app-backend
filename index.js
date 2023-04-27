1bdea650dd785a9a5132f9e605199642995fac85
Simon Tran <thanhnhantran1501@gmail.com>
2023-04-27 00:45:14 -0700

    update the passportConfig.js

--- index.js
+++ index.js
@@ -13,13 +13,8 @@ const logoutRoute = require("./routes/logoutRoute");
 const SESSION_SECRET = process.env.SESSION_SECRET;
 const googleAuthRoute = require("./routes/googleAuthRoute");
 //GOOGLE AUTHENTICATION NEDDED LIBRARIES
-// const passport = require("./config/passportConfig");
-const passport = require("passport");
+const passport = require("./config/passportConfig");
 const expressSession = require("express-session");
-const User = require("./models/usersModel");
-const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
-const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
-const GoogleStrategy = require("passport-google-oauth20").Strategy;
 
 //SET UP CORS MIDDLEWARE
 app.use(
@@ -59,53 +54,6 @@ app.use(passport.initialize());
 
 // PASSPORT SESSION CALL THE DESERIALIZEUSER FUNCTION AND PASS THE USER ID TO THE PARAMETER OF THIS FUNCTION
 app.use(passport.session());
-
-//IMPLEMENT GOOGLE STRATEGY
-passport.use(
-  new GoogleStrategy(
-    {
-      clientID: GOOGLE_CLIENT_ID,
-      clientSecret: GOOGLE_CLIENT_SECRET,
-      callbackURL: `https://nasa-app-backend.vercel.app/auth/google/callback`,
-    },
-    function (_accessToken, _refreshToken, profile, done) {
-      const { id: google_id, name: nameObj } = profile;
-
-      //CHECK TO SEE IF THIS USER IS IN DATABASE
-      User.findOne({ google_id: google_id, username: nameObj.givenName })
-        .then((user) => {
-          //IF THIS USER EXISTS IN DATABASE
-          if (user) {
-            done(null, { _id: user._id });
-          }
-
-          //IF THIS IS A NEW USER
-          else {
-            User.create({ google_id: google_id, username: nameObj.givenName })
-              .then((res) => {
-                done(null, { _id: res._id });
-              })
-              .catch((e) => console.log(`Error creating a user ${e}`));
-          }
-        })
-        .catch((e) => console.log(`Error fetching a user ${e}`));
-    }
-  )
-);
-
-//SET UP SERIALIZEUSER
-passport.serializeUser((userObj, done) => {
-  done(null, userObj._id);
-});
-
-//SSET UP DESERIALIZEUSER
-passport.deserializeUser((userId, done) => {
-  User.findOne({ _id: userId })
-    .then((user) => done(null, { _id: user._id, username: user.username }))
-    .catch((e) => {
-      console.log("Error finding user");
-    });
-});
 
 //CONNECT TO THE MONGODB DATABASE
 handleConnectMongoDBServer();
